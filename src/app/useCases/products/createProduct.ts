d857169c124a5f907330e7076db62b3fe2e78a1b
Luiggi Abdiel <luiggiabdieldev@gmail.com>
2023-03-29 14:33:11 -0300

    feat: finalizando uso de casos dos Product

--- src/app/useCases/products/createProduct.ts
+++ src/app/useCases/products/createProduct.ts
@@ -1,8 +1,21 @@
 import { Request, Response } from 'express';
+import { Product } from '../../models/Product';
 
 export async function createProduct(req: Request, res: Response) {
   try {
-    console.log(req.body);
+    const imagePath = req.file?.filename;
+    const { name, description, price, category, ingredients } = req.body;
+
+    const product = await Product.create({
+      name,
+      imagePath,
+      description,
+      price: Number(price),
+      category,
+      ingredients: JSON.parse(ingredients),
+    });
+
+    res.status(201).json(product);
   } catch(error) {
     console.log(error);
     res.sendStatus(500);
